70587e00d54becfbbbf39d773e14075091c7a77b
cancub <cancub@live.com>
2016-02-26 21:22:57 -0500

    adding functions for setting values and using a rudimentary run function to show that it can effectively retrieve frames from frame generating process

--- host.cpp
+++ host.cpp
@@ -4,27 +4,85 @@
 #include <cmath>
 #include <algorithm>
 #include <unistd.h>
-#include <cstring>
 #include <string>
 #include "frame_generators.h"
+#include "frames.h"
 #include "host.h"
+#include <chrono>
 
 using namespace std;
 
 Host::Host() {
 	ip.reserve(4);
-	mac_addr.reserve(6);
+	mac.reserve(6);
 }
 
-Host::Host(string ip_addr, string mac_addr) {
+Host::Host(std::string ip_addr, std::string mac_addr) {
 	ip = ip_addr;
+	mac = mac_addr;
+	frame = new Frame(mac);
+	frame_generator = new Poisson(10);
+}
+
+Host::~Host() {
+	delete frame;
+	delete frame_generator;
+}
+
+// void Host::initialize() {}
+
+void Host::run() {
+	// get frame delay
+	// loop until then
+	// in loop, check to see if mutex is grabbed
+	// when grabbed, retrieve frame from interface and
+	// process it
+	// after loop finishes, grab mutex and put on interface
+	// go back to beginning
+
+	std::chrono::duration<double> delay_us;
+	double total_t = 0.0;
+	auto start_time = std::chrono::high_resolution_clock::now();
+
+	while (1) {
+		start_time = std::chrono::high_resolution_clock::now();
+		delay_us = std::chrono::duration<double>(frame_generator->interarrival_time());
+
+		while (std::chrono::high_resolution_clock::now() - start_time < delay_us) {
+			// if(/*mutex has been grabbed*/) {
+			// 	/*wait on mutex*/
+			// 	/*grab mutex*/
+			// 	copy frame
+			// 	/*clear frame*/
+			// 	/*release mutex*/
+			// 	/*print copy*/
+			// }
+		}
+
+		total_t += delay_us.count();
+
+		std::cout << "Frame arrived at " << total_t << " s" << std::endl;
+		/*grab mutex*/
+		/*put put frame on link*/
+		/*release mutex*/
+
+	}
+}
+
+std::string Host::get_ip() {return ip;}
+
+std::string Host::get_mac() {return mac;}
+
+void Host::set_ip(std::string ip_addr) {
+	ip = ip_addr;
+}
+
+void Host::set_mac(std::string mac_addr) {
 	mac = mac_addr;
 }
 
-void Host::initialize() {}
+int main(void) {
 
-void Host::start() {}
-
-string Host::ip() {return ip;}
-
-string Host::mac() {return mac;}
+	Host alice("192.168.0.188", "aa:bb:cc:dd:ee:ff");
+	alice.run();
+}
--- host.h
+++ host.h
@@ -1,23 +1,27 @@
 #ifndef HOST_H
 #define HOST_H
 
-#include <stdlib.h>
 #include <string>
-#include <vector>
 #include "frame_generators.h"
+#include "frames.h"
 
 class Host{
 	public:
 		Host();
-		Host(string, string);
-		void initialize();
-		void start();
-		string ip();
-		string mac();
+		Host(std::string, std::string);
+		~Host();
+		// void initialize();
+		void run();
+		std::string get_ip();
+		std::string get_mac();
+		void set_ip(std::string);
+		void set_mac(std::string);
 	private:
-		Poisson frame_generator;
-		string ip;
-		string mac;
+		Poisson* frame_generator;
+		Frame* frame;
+		Frame* interface;
+		std::string ip;
+		std::string mac;
 };
 
 #endif
