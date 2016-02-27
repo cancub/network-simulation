fb6a99d2f8afe2e7b2b9628d1c4d43a2c2aefcd0
cancub <cancub@live.com>
2016-02-27 10:26:26 -0500

    at this point host objects are now able to communicate with one another over a (mutex) link as threads. HUZZAH!

--- host.cpp
+++ host.cpp
@@ -17,21 +17,23 @@ Host::Host() {
 	mac.reserve(6);
 }
 
-Host::Host(std::string ip_addr, std::string mac_addr) {
+Host::Host(std::string ip_addr, std::string mac_addr, std::mutex* if_mutex, Frame* iface) {
 	ip = ip_addr;
 	mac = mac_addr;
-	frame = new Frame(mac);
+	interface_mutex = if_mutex;
+	interface = iface;
+	runtime = 0.0;
 	frame_generator = new Poisson(10);
 }
 
 Host::~Host() {
-	delete frame;
+	// delete interface;
 	delete frame_generator;
 }
 
 // void Host::initialize() {}
 
-void Host::run() {
+void Host::run(std::string dst_mac) {
 	// get frame delay
 	// loop until then
 	// in loop, check to see if mutex is grabbed
@@ -40,31 +42,37 @@ void Host::run() {
 	// after loop finishes, grab mutex and put on interface
 	// go back to beginning
 
-	std::chrono::duration<double> delay_us;
-	double total_t = 0.0;
-	auto start_time = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> delay_us; // the time that the code should wait for
+	auto start_time = std::chrono::high_resolution_clock::now(); // the time to test if code has waited
+																// long enough
 
 	while (1) {
+		//update start time
 		start_time = std::chrono::high_resolution_clock::now();
+		// get the interarrival time for the next frame
 		delay_us = std::chrono::duration<double>(frame_generator->interarrival_time());
-
+		// loop until its time to send the frame
 		while (std::chrono::high_resolution_clock::now() - start_time < delay_us) {
-			// if(/*mutex has been grabbed*/) {
-			// 	/*wait on mutex*/
-			// 	/*grab mutex*/
-			// 	copy frame
-			// 	/*clear frame*/
-			// 	/*release mutex*/
-			// 	/*print copy*/
-			// }
+			// check if the host on the other end of the link has locked the mutex
+			if (interface_mutex->try_lock()) {
+				// release the mutex if the other host has not locked it
+				// since it isn't time to send the frame yet and there's nothing to
+				// read
+				interface_mutex->unlock();
+			}
+			else {
+				// process the frame
+				process_frame();			
+			}
 		}
 
-		total_t += delay_us.count();
+		// now that the code is done waiting, it can attempt to send the frame
 
-		std::cout << "Frame arrived at " << total_t << " s" << std::endl;
-		/*grab mutex*/
-		/*put put frame on link*/
-		/*release mutex*/
+		// increase the total time so far
+		runtime += delay_us.count();
+		send_frame(rand() % 1500, dst_mac);
+
+
 
 	}
 }
@@ -81,8 +89,51 @@ void Host::set_mac(std::string mac_addr) {
 	mac = mac_addr;
 }
 
-int main(void) {
-
-	Host alice("192.168.0.188", "aa:bb:cc:dd:ee:ff");
-	alice.run();
+void Host::set_interface(Frame* frame_interface) {
+	interface = frame_interface;
 }
+
+void Host::set_mutex(std::mutex* if_mutex) {
+	interface_mutex = if_mutex;
+}
+
+void Host::send_frame(int frame_size, std::string dst_mac) {	
+	// recursively attempt to put the frame on the link
+	// just in case there's a conflict
+	if (interface_mutex->try_lock()) {
+		interface->set_src_mac(mac);
+		interface->set_dst_mac(dst_mac);
+		interface->set_frame_size(frame_size);
+		std::cout << mac << " sent frame of " << interface-> get_frame_size() << " bytes to " 
+			<< dst_mac << " at " << runtime << " s" << std::endl;
+		interface_mutex->unlock();		
+	}
+	else {
+		send_frame(frame_size, dst_mac);
+	}
+}
+
+void Host::process_frame() {
+	// hang here to lock the mutex as soon as the other host is done with it
+	interface_mutex->lock();
+	// check on the link to see if the frame is for this hos
+	if (interface->get_dst_mac() == mac) {
+		// print some details about the frame
+		cout << mac << ": frame of " << interface->get_frame_size() << " bytes received from " 
+			<< interface->get_src_mac() << endl;
+		// get rid of the frame to signify that it was received
+		interface->erase();
+	}	
+	// give the mutex back
+	interface_mutex->unlock();	
+}
+
+// int main(void) {
+
+// 	Host alice("192.168.0.188", "aa:bb:cc:dd:ee:ff");
+// 	std::mutex test_mutex;
+// 	Frame interface;
+// 	alice.set_mutex(&test_mutex);
+// 	alice.set_interface(&interface);
+// 	alice.run("00:11:22:33:44:55");
+// }
--- host.h
+++ host.h
@@ -2,26 +2,33 @@
 #define HOST_H
 
 #include <string>
+#include <mutex>
 #include "frame_generators.h"
 #include "frames.h"
 
 class Host{
 	public:
+		// void operator () (std::string, std::string, std::mutex*, Frame*);
 		Host();
-		Host(std::string, std::string);
+		Host(std::string, std::string, std::mutex*, Frame*);
 		~Host();
 		// void initialize();
-		void run();
+		void run(std::string);
 		std::string get_ip();
 		std::string get_mac();
 		void set_ip(std::string);
 		void set_mac(std::string);
+		void set_interface(Frame*);
+		void set_mutex(std::mutex*);
 	private:
+		void send_frame(int, std::string);
+		void process_frame();
 		Poisson* frame_generator;
-		Frame* frame;
 		Frame* interface;
 		std::string ip;
 		std::string mac;
+		std::mutex* interface_mutex;
+		double runtime;
 };
 
 #endif
