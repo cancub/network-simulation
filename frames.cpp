5c6e92cf6fb52aa1c6e591f3aac08ea26557367e
cancub <cancub@live.com>
2016-02-26 21:20:45 -0500

    adding get and set functions for addressing and frame size, modified naming

--- frames.cpp
+++ frames.cpp
@@ -1,22 +1,59 @@
-#include <stdlib.h>
 #include <string>
 #include "frames.h"
 
 
 Frame::Frame() {
-	source_address = "";
-	destination_address = "";
-	size = 0;
+	source_mac = "";
+	destination_mac = "";
+	frame_size = 0;
 }
 
-Frame::Frame(string src, string dest, size_t length) {
-	source_address = src;
-	destination_address = dst;
-	size = length;
+Frame::Frame(std::string src) {
+	source_mac = src;
+	destination_mac = "";
+	frame_size = 0;
 }
 
-string Frame::src() { return source_address; }
+Frame::Frame(std::string src, size_t length) {
+	source_mac = src;
+	destination_mac = "";
+	frame_size = length;
+}
 
-string Frame::dst() { return destination_address; }
+Frame::Frame(std::string src, std::string dst, size_t length) {
+	source_mac = src;
+	destination_mac = dst;
+	frame_size = length;
+}
+
+Frame::~Frame() {}
+
+std::string Frame::get_src_mac() { return source_mac; }
+
+std::string Frame::get_dst_mac() { return destination_mac; }
+
+std::string Frame::get_src_ip() { return source_ip; }
+
+std::string Frame::get_dst_ip() { return destination_ip; }
 		
-size_t Frame::size() {return size;}
+size_t Frame::get_frame_size() {return frame_size;}
+
+void Frame::set_src_mac(std::string mac) {
+	source_mac = mac;
+}
+
+void Frame::set_dst_mac(std::string mac) {
+	destination_mac = mac;
+}
+
+void Frame::set_src_ip(std::string ip) {
+	source_ip = ip;
+}
+
+void Frame::set_dst_ip(std::string ip) {
+	destination_ip = ip;
+}
+
+void Frame::set_frame_size(size_t length) {
+	frame_size = length;
+}
--- frames.h
+++ frames.h
@@ -7,14 +7,26 @@
 class Frame {
 	public:
 		Frame();
-		Frame(string, string, size_t);
-		string src();
-		string dst();
-		size_t size();
+		Frame(std::string);
+		Frame(std::string, size_t);
+		Frame(std::string, std::string, size_t);
+		~Frame();
+		std::string get_src_mac();
+		std::string get_dst_mac();
+		std::string get_src_ip();
+		std::string get_dst_ip();
+		size_t get_frame_size();
+		void set_src_mac(std::string);
+		void set_dst_mac(std::string);
+		void set_src_ip(std::string);
+		void set_dst_ip(std::string);
+		void set_frame_size(size_t);
 	private:
-		string source_address;
-		string destination_address;
-		size_t size;
+		std::string source_mac;
+		std::string destination_mac;
+		std::string source_ip;
+		std::string destination_ip;
+		size_t frame_size;
 };
 
 #endif
