bd377b8a5bd00d932c8dd189b22cd7d9784eda0e
cancub <cancub@live.com>
2016-02-28 00:50:33 -0500

    test file for a switch and two hosts

--- test.cpp
+++ test.cpp
@@ -1,23 +1,39 @@
-#include "host.h"
 #include <stdlib.h>
 #include <thread>
 #include <mutex>
+#include <iostream>
+#include "host.h"
 #include "frames.h"
+#include "addressing.h"
+#include "networking_devices.h"
 
 using namespace std;
 
 int main(void) {
-	std::mutex test_mutex;
-	Frame interface;
-	Host alice("192.168.0.188", "aa:bb:cc:dd:ee:ff", &test_mutex, &interface);
-	Host bob("192.168.0.186", "00:11:22:33:44:55", &test_mutex, &interface);
+	srand (time(NULL));
 
-	alice.set_mutex(&test_mutex);
-	bob.set_mutex(&test_mutex);
-	alice.set_interface(&interface);
-	bob.set_interface(&interface);
-	thread test_alice(&Host::run,&alice, "00:11:22:33:44:55");
-	thread test_bob(&Host::run, &bob, "aa:bb:cc:dd:ee:ff");
+	int i = 0;
+
+	std::string alice_mac = random_mac();
+	std::string bob_mac = random_mac();
+	Host alice(random_ip(), alice_mac, "alice");
+	Host bob(random_ip(), bob_mac, "bob");
+	Switch my_switch("star_switch");
+
+	// std::cout << i++ << std::endl;
+
+	my_switch.plug_in_device(&alice);
+	my_switch.plug_in_device(&bob);
+
+	// std::cout << i++ << std::endl;
+	thread test_switch(&Switch::run, &my_switch);
+	// std::cout << i++ << std::endl;
+	thread test_alice(&Host::run,&alice, bob_mac);
+	// std::cout << i++ << std::endl;
+	thread test_bob(&Host::run, &bob, alice_mac);
+
+	// std::cout << i++ << std::endl;
+	test_switch.join();
 	test_alice.join();
 	test_bob.join();
 }
