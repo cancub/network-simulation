37922cc58687c90453802ade7a5240942c78b509
cancub <cancub@live.com>
2016-08-30 11:33:48 -0400

    minor modifications to frames for copy function

--- frames.cpp
+++ frames.cpp
@@ -3,27 +3,27 @@
 
 
 Frame::Frame() {
-	source_mac = "";
-	destination_mac = "";
-	frame_size = 0;
+    source_mac = "";
+    destination_mac = "";
+    frame_size = 0;
 }
 
 Frame::Frame(std::string src) {
-	source_mac = src;
-	destination_mac = "";
-	frame_size = 0;
+    source_mac = src;
+    destination_mac = "";
+    frame_size = 0;
 }
 
 Frame::Frame(std::string src, size_t length) {
-	source_mac = src;
-	destination_mac = "";
-	frame_size = length;
+    source_mac = src;
+    destination_mac = "";
+    frame_size = length;
 }
 
 Frame::Frame(std::string src, std::string dst, size_t length) {
-	source_mac = src;
-	destination_mac = dst;
-	frame_size = length;
+    source_mac = src;
+    destination_mac = dst;
+    frame_size = length;
 }
 
 Frame::~Frame() {}
@@ -35,33 +35,44 @@ std::string Frame::get_dst_mac() { return destination_mac; }
 std::string Frame::get_src_ip() { return source_ip; }
 
 std::string Frame::get_dst_ip() { return destination_ip; }
-		
+        
 size_t Frame::get_frame_size() {return frame_size;}
 
 void Frame::set_src_mac(std::string mac) {
-	source_mac = mac;
+    source_mac = mac;
 }
 
 void Frame::set_dst_mac(std::string mac) {
-	destination_mac = mac;
+    destination_mac = mac;
 }
 
 void Frame::set_src_ip(std::string ip) {
-	source_ip = ip;
+    source_ip = ip;
 }
 
 void Frame::set_dst_ip(std::string ip) {
-	destination_ip = ip;
+    destination_ip = ip;
 }
 
 void Frame::set_frame_size(size_t length) {
-	frame_size = length;
+    frame_size = length;
 }
 
 void Frame::erase() {
-	frame_size = 0;
-	source_mac = "";
-	source_ip = "";
-	destination_mac = "";
-	destination_ip = "";
+    frame_size = 0;
+    source_mac = "";
+    source_ip = "";
+    destination_mac = "";
+    destination_ip = "";
+}
+
+Frame* Frame::copy() {
+    Frame* frame_copy = new Frame();
+    frame_copy->set_src_ip(source_ip);
+    frame_copy->set_src_mac(source_mac);
+    frame_copy->set_dst_ip(destination_ip);
+    frame_copy->set_dst_mac(destination_mac);
+    frame_copy->set_frame_size(frame_size);
+
+    return frame_copy;
 }
--- frames.h
+++ frames.h
@@ -10,30 +10,31 @@ just a pointer to a frame on a link?
 */
 
 class Frame {
-	public:
-		Frame(); // default constructor
-		Frame(std::string); // constructor with the source MAC address
-		Frame(std::string, size_t); // constructor with the source MAC address and the length of the frame
-		Frame(std::string, std::string, size_t); // constructor with the source MAC address, 
-												// the destination address and the length of the frame
-		~Frame();
-		std::string get_src_mac();
-		std::string get_dst_mac();
-		std::string get_src_ip();
-		std::string get_dst_ip();
-		size_t get_frame_size();
-		void set_src_mac(std::string);
-		void set_dst_mac(std::string);
-		void set_src_ip(std::string);
-		void set_dst_ip(std::string);
-		void set_frame_size(size_t);
-		void erase();	// reset the frame to be blank
-	private:
-		std::string source_mac;
-		std::string destination_mac;
-		std::string source_ip;
-		std::string destination_ip;
-		size_t frame_size;
+    public:
+        Frame(); // default constructor
+        Frame(std::string); // constructor with the source MAC address
+        Frame(std::string, size_t); // constructor with the source MAC address and the length of the frame
+        Frame(std::string, std::string, size_t); // constructor with the source MAC address, 
+                                                // the destination address and the length of the frame
+        ~Frame();
+        std::string get_src_mac();
+        std::string get_dst_mac();
+        std::string get_src_ip();
+        std::string get_dst_ip();
+        size_t get_frame_size();
+        void set_src_mac(std::string);
+        void set_dst_mac(std::string);
+        void set_src_ip(std::string);
+        void set_dst_ip(std::string);
+        void set_frame_size(size_t);
+        void erase();   // reset the frame to be blank
+        Frame* copy();
+    private:
+        std::string source_mac;
+        std::string destination_mac;
+        std::string source_ip;
+        std::string destination_ip;
+        size_t frame_size;
 };
 
 #endif
