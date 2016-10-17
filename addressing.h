189bab0690d3ea63de7fb25a377273a20cd4db51
cancub <cancub@live.com>
2016-10-17 00:15:28 -0400

    more conformity between functions

--- addressing.h
+++ addressing.h
@@ -6,19 +6,23 @@
 // #include <vector>
 #include <string>
 #include <cstdint>
+#include <vector>
 
-std::vector<uint8_t> random_mac();
-std::vector<uint8_t> uniform_mac(uint8_t);
-std::vector<uint8_t> broadcast_mac();
-std::vector<uint8_t> random_ip();
-std::vector<uint8_t> uniform_ip(uint8_t);
-std::vector<uint8_t> broadcast_ip();
+std::vector<uint8_t> create_mac(uint8_t,uint8_t,uint8_t,uint8_t,uint8_t,uint8_t);
+std::vector<uint8_t> create_random_mac();
+std::vector<uint8_t> create_uniform_mac(uint8_t);
+std::vector<uint8_t> create_broadcast_mac();
+
+uint32_t create_ip(uint8_t,uint8_t, uint8_t, uint8_t);
+uint32_t create_random_ip();
+uint32_t create_broadcast_ip();
 
 int compare_macs(std::vector<uint8_t>, std::vector<uint8_t>);
-int compare_ips(std::vector<uint8_t>, std::vector<uint8_t>);
+int compare_ips(uint32_t, uint32_t);
 int is_broadcast(std::vector<uint8_t>);
+int is_broadcast(uint32_t);
 
 std::string mac_to_string(std::vector<uint8_t>);
-std::string ip_to_string(std::vector<uint8_t>);
+std::string ip_to_string(uint32_t);
 
 #endif
